a0e6ae2a1c3356b00c5eba56ea4560197d268e37
NunoPinto <167584289+NETERNOT@users.noreply.github.com>
2025-04-29 11:41:04 +0100

    Minor tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 
 
 import './App.css'
-import SortingGame from './pages/Sorting/Sorting'
+//import SortingGame from './pages/sorting/Sorting'
+//import MemoryGame from './pages/memory/Memory'
+import CustomSortingGame from './pages/customSorting/customSorting'
 
 function App() {
 
   return (
     <>
-      <SortingGame />
+      <CustomSortingGame />
     </>
   )
 }
